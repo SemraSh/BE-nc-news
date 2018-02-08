f73b0a17fb025dd8defc65de615d09e276a875b4
Semra Dora <semra.refkaewa@gmail.com>
2018-02-08 17:02:19 +0000

    change env file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const apiRouter = require('./routes/apiRouter')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
-const DB = process.env
+const {DB_URL} = process.env
 
 mongoose.connect(DB_URL)
 
