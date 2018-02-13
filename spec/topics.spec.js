640e798e8f258a15575f49f7dc9805ea14bbbeb9
Semra Dora <semra.refkaewa@gmail.com>
2018-02-13 18:03:29 +0000

    add test for addNewArticle on  /topics router

--- spec/topics.spec.js
+++ spec/topics.spec.js
@@ -7,6 +7,7 @@ const db = process.env.DB_URL_TEST;
 mongoose.Promise = Promise;
 
 describe('Topics', () => {
+	let data;
 	before(() => {
 		const checkConnection = mongoose.connection.readyState === 0 ? mongoose.connect(db) : Promise.resolve();
 		return checkConnection
@@ -14,7 +15,10 @@ describe('Topics', () => {
 				return mongoose.connection.dropDatabase();
 			})
 			.then(saveTestData)
-			.then(savedData => savedData);
+			.then(savedData => {
+				data = savedData;
+				return data;
+			});
 	});
 	after(done => { mongoose.disconnect(), done(); });
 
@@ -27,6 +31,37 @@ describe('Topics', () => {
 				expect(topics).to.be.an('array');
 				expect(topics.length).to.equal(3);
 				expect(topics[0].title).to.equal('Football');
+			});
+	});
+	it('"GET /topics/:topic/articles/" returns all the articles for one topic', () => {
+		const topic = data.topics[0].slug;
+		return request
+			.get(`/topics/${topic}/articles`)
+			.expect(200)
+			.then(res => {
+				const articlesByTopic = res.body;
+				expect(articlesByTopic[0].belongs_to).to.equal(`${topic}`);
+				expect(articlesByTopic.length).to.equal(1);
+				expect(Object.keys(articlesByTopic[0]).length).to.equal(6);
+			});
+	});
+	it('"POST /topics/:topic/articles" creates a new article under the requested topic', ()=>{
+		const topic = data.topics[1].slug;
+		const article = {		
+			belongs_to: topic,
+			title: 'Test article',
+			body: 'This is a test article' ,
+			created_by: 'creator'};
+		return request
+			.post(`/topics/${topic}/articles`)
+			.send(article)
+			.expect(201)
+			.then(res => {
+				const newArticle = res.body;
+				expect(newArticle.title).to.equal(article.title);
+				expect(newArticle.body).to.equal(article.body);
+				expect(newArticle.created_by).to.equal(article.created_by);
+				expect(newArticle.belongs_to).to.equal(`${topic}`);
 			});
 	});
 });
