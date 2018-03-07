28e4b0998fb70a31b4252a42b15158acd2dead05
Semra Dora <semra.refkaewa@gmail.com>
2018-03-07 18:42:04 +0000

    add url images

--- seed/data/user_data.js
+++ seed/data/user_data.js
@@ -1,8 +1,8 @@
 module.exports = [
-	{username: 'tickle122', name: 'Tom Tickle', avatar_url: 'https://images-na.ssl-images-amazon.com/images/I/51zTubVDZoL.jpg'},
-	{username: 'grumpy19', name: 'Paul Grump', avatar_url: 'https://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=8941715'},
-	{username: 'happyamy2016', name: 'Amy Happy', avatar_url: 'https://vignette.wikia.nocookie.net/mrmen/images/3/31/Mr.Happy.jpg/revision/latest?cb=20090711074927'},
+	{username: 'tickle122', name: 'Tom Tickle', avatar_url: 'https://i.pinimg.com/originals/1b/3d/72/1b3d72687c4fc8a435d3b8f59d2a27b3.jpg'},
+	{username: 'grumpy19', name: 'Paul Grump', avatar_url: 'https://i.pinimg.com/originals/19/ff/7c/19ff7cc3c263f9b7adf1819bdc5ccd84.jpg'},
+	{username: 'happyamy2016', name: 'Amy Happy', avatar_url: 'https://i.pinimg.com/736x/79/97/1d/79971d820d13dc6ba15e7312354b7830--koala-illustration-animal-illustrations.jpg'},
 	{username: 'cooljmessy', name: 'Peter Messy', avatar_url: 'https://i.pinimg.com/564x/7f/e0/1c/7fe01c682d8187e089c922089e0deb25.jpg'},
-	{username: 'weegembump', name: 'Gemma Bump', avatar_url: 'http://payload484.cargocollective.com/1/2/76113/11981602/Mr-bump-low_670.jpg'},
-	{username: 'jessjelly', name: 'Jess Jelly', avatar_url: 'https://www.mrmen.com/wp-content/uploads/2016/06/mrjelly-512.jpg'}
+	{username: 'weegembump', name: 'Gemma Bump', avatar_url: 'https://i.pinimg.com/originals/e4/60/3d/e4603d9e0512bcf1004fca71ae1373bd.jpg'},
+	{username: 'jessjelly', name: 'Jess Jelly', avatar_url: 'https://i.pinimg.com/originals/ef/12/49/ef1249998186081789042c2bfb266a6d.jpg'}
 ];
