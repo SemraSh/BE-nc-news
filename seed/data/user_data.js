958eacde9d63f38123b3fc78a20b34aa0e712fc3
Semra Dora <semra.refkaewa@gmail.com>
2018-03-07 18:23:45 +0000

    change user images in the database

--- seed/data/user_data.js
+++ seed/data/user_data.js
@@ -1,8 +1,8 @@
 module.exports = [
-	{username: 'tickle122', name: 'Tom Tickle', avatar_url: 'https://img00.deviantart.net/e6bf/i/2013/003/a/1/mr__tickle_by_emretsim-d5q2taq.jpg'},
-	{username: 'grumpy19', name: 'Paul Grump', avatar_url: 'https://shop.mrmen.com/wp-content/themes/mrmen/assets/characterpic-square-mr%20grumpy.jpg'},
-	{username: 'happyamy2016', name: 'Amy Happy', avatar_url: 'http://www.saturaterecords.info/wp-content/uploads/25-trending-grizzly-bear-drawing-ideas-on-pinterest-bear-grizzly-bear-artwork.jpg'},
+	{username: 'tickle122', name: 'Tom Tickle', avatar_url: 'https://images-na.ssl-images-amazon.com/images/I/51zTubVDZoL.jpg'},
+	{username: 'grumpy19', name: 'Paul Grump', avatar_url: 'https://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=8941715'},
+	{username: 'happyamy2016', name: 'Amy Happy', avatar_url: 'https://vignette.wikia.nocookie.net/mrmen/images/3/31/Mr.Happy.jpg/revision/latest?cb=20090711074927'},
 	{username: 'cooljmessy', name: 'Peter Messy', avatar_url: 'https://i.pinimg.com/564x/7f/e0/1c/7fe01c682d8187e089c922089e0deb25.jpg'},
 	{username: 'weegembump', name: 'Gemma Bump', avatar_url: 'http://payload484.cargocollective.com/1/2/76113/11981602/Mr-bump-low_670.jpg'},
-	{username: 'jessjelly', name: 'Jess Jelly', avatar_url: 'https://vignette.wikia.nocookie.net/mrmen/images/7/70/Mr-jelly-5a.png/revision/latest?cb=20180104121201'}
+	{username: 'jessjelly', name: 'Jess Jelly', avatar_url: 'https://www.mrmen.com/wp-content/uploads/2016/06/mrjelly-512.jpg'}
 ];
